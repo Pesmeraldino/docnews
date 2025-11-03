3aec8efe1deaf43b36bde659db6b11db6927fdff
Pesmeraldino <pedroesmeraldino22@gmail.com>
2025-11-03 19:36:32 -0300

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultMigrationsOpitions = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
